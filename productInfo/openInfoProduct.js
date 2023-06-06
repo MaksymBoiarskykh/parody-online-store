e2ad179dc153927ad6290ce0268808bf98969ae4
Maksym Boiarskykh <Boarskihmaks18@gmail.com>
2023-06-06 13:20:27 +0300

    modificate fake store

--- productInfo/openInfoProduct.js
+++ productInfo/openInfoProduct.js
@@ -3,7 +3,10 @@ import createInfo from "./createInforProduct.js";
 // створюю подію відкриття інформації при натисканні
 function openInfoProduct(product, infoProduct, infoBlockClass) {
   const infoContainer = document.querySelector(infoBlockClass);
-  product.addEventListener("click", createInfo(infoProduct, infoContainer));
+
+  product.addEventListener("click", () => {
+    createInfo(infoProduct, infoContainer);
+  });
 }
 
 export default openInfoProduct;
